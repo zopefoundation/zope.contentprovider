4d543ee500c9403acfdb62429c9441b1984f5082
Christian Theune <ct@gocept.com>
2007-04-22 10:56:57 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.contentprovider',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.contentprovider',
       license='ZPL 2.1',
       description='Zope contentprovider',
