e762536dfa80c6506e89e0817e836ad228b2919f
Tres Seaver <tseaver@palladion.com>
2010-05-25 14:09:27 +0000

    svb

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.contentprovider',
-      version = '3.7.2',
+      version = '3.7.3dev',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Content Provider Framework for Zope Templates',
