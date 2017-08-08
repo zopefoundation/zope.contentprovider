0785524951273a2e105da55ad3d59fa50819216f
Jason Madden <jamadden@gmail.com>
2017-08-08 06:15:53 -0500

    De-dup test requires and re-indent setup.py keywords.

--- setup.py
+++ setup.py
@@ -25,62 +25,65 @@ def read(*rnames):
     with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
         return f.read()
 
-setup(name='zope.contentprovider',
-      version='4.1.0.dev0',
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.org',
-      description='Content Provider Framework for Zope Templates',
-      long_description=(
-          read('README.rst')
-          + '\n\n' +
-          read('src', 'zope', 'contentprovider', 'README.rst')
-          + '\n\n' +
-          read('CHANGES.rst')
-      ),
-      keywords="zope3 content provider",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope3',
-      ],
-      url='http://github.com/zopefoundation/zope.contentprovider',
-      license='ZPL 2.1',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['zope'],
-      extras_require={
-          'test': [
-              'zope.browserpage>=3.12',
-              'zope.testing',
-              'zope.testrunner',
-          ],
-      },
-      install_requires=[
-          'setuptools',
-          'zope.component',
-          'zope.event',
-          'zope.interface',
-          'zope.location',
-          'zope.publisher',
-          'zope.schema',
-          'zope.tales',
-      ],
-      include_package_data=True,
-      tests_require=['zope.testing', 'zope.browserpage'],
-      test_suite='zope.contentprovider.tests.test_suite',
-      zip_safe=False,
+TESTS_REQUIRE = [
+    'zope.browserpage>=3.12',
+    'zope.testing',
+    'zope.testrunner',
+]
+
+setup(
+    name='zope.contentprovider',
+    version='4.1.0.dev0',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='Content Provider Framework for Zope Templates',
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('src', 'zope', 'contentprovider', 'README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+    ),
+    keywords="zope3 content provider",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3',
+    ],
+    url='http://github.com/zopefoundation/zope.contentprovider',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope'],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    install_requires=[
+        'setuptools',
+        'zope.component',
+        'zope.event',
+        'zope.interface',
+        'zope.location',
+        'zope.publisher',
+        'zope.schema',
+        'zope.tales',
+    ],
+    include_package_data=True,
+    tests_require=TESTS_REQUIRE,
+    test_suite='zope.contentprovider.tests.test_suite',
+    zip_safe=False,
 )
