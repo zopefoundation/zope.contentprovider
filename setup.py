f320044d19676a68e179ff34c75fc14d995aad3e
Marius Gedminas <marius@gedmin.as>
2018-10-19 00:59:01 +0300

    Switch to new Framework :: Zope :: 3 classifier
    See https://github.com/zopefoundation/z3c.authviewlet/pull/6#discussion_r226414566
    for the background behind this.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope3',
+        'Framework :: Zope :: 3',
     ],
     url='https://github.com/zopefoundation/zope.contentprovider',
     license='ZPL 2.1',
