58ddf1a5b725685104433416305a07e74bf83d05
Michael Howitz <icemac@gmx.net>
2025-09-12 09:01:46 +0200

    Back to development: 7.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.contentprovider',
-    version='7.0',
+    version='7.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='Content Provider Framework for Zope Templates',
