5b8f75aacf2365f0c2a36037d4837eceb53a2e53
Michael Howitz <icemac@gmx.net>
2026-02-16 08:42:22 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -17,75 +17,8 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 """Setup for zope.contentprovider package"""
-
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-TESTS_REQUIRE = [
-    'zope.browserpage>=3.12',
-    'zope.testing',
-    'zope.testrunner >= 6.4',
-]
-
-
-setup(
-    name='zope.contentprovider',
-    version='7.1.dev0',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    description='Content Provider Framework for Zope Templates',
-    long_description=(
-        read('README.rst')
-        + '\n\n' +
-        read('CHANGES.rst')
-    ),
-    keywords="zope3 content provider",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope :: 3',
-    ],
-    url='https://github.com/zopefoundation/zope.contentprovider',
-    license='ZPL-2.1',
-    python_requires='>=3.10',
-    extras_require={
-        'test': TESTS_REQUIRE,
-        'docs': [
-            'Sphinx',
-            'repoze.sphinx.autointerface',
-        ],
-    },
-    install_requires=[
-        'setuptools',
-        'zope.component',
-        'zope.event',
-        'zope.interface >= 3.8',
-        'zope.location',
-        'zope.publisher',
-        'zope.schema',
-        'zope.tales',
-    ],
-    include_package_data=True,
-    zip_safe=False,
-)
+# See pyproject.toml for package metadata
+setup()
